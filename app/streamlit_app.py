b5b501174a0738342d1a2ddec7c84908cf6c119c
gigasamkharadze <samkharadze.giga@kiu.edu.ge>
2025-04-04 10:36:31 +0400

    remove comments

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,16 +1,12 @@
 import streamlit as st
 import requests
 
-# Streamlit app title
 st.title("Code Plagiarism Detector")
 
-# Text area for code input
 code_input = st.text_area("Enter your code here:")
 
-# Submit button
 if st.button("Submit"):
     if code_input:
-        # Send request to the API
         response = requests.post("http://localhost:8001/check", json={"code": code_input})
         
         if response.status_code == 200:
